056e8a1f40e445dfd8483f6fa25665bd1e1d5046
David Tenni <davidctenni@gmail.com>
2022-07-10 17:00:08 +1000

    added: basic setup files for site

---
+++ scripts/heart.js
@@ -0,0 +1,56 @@
+
+let x = 0;
+setInterval(function(){ 
+    if (x <= 5){
+        x = x + 1; 
+
+    } else{
+        x = 0;
+    }
+    switch (x){
+        case x = 1:
+            heart.style.color = "magenta";
+
+            break;
+        case x = 2:
+            heart.style.color = "maroon";
+
+            break;
+        case x = 3:
+            heart.style.color = "blue";
+
+            break;
+        case x = 4:
+            heart.style.color = "green";
+
+            break;
+        case x = 5:
+            heart.style.color = "coral";
+
+            break;
+        case x = 6:
+            heart.style.color = "purple";
+
+            x = 0;
+            break;
+    default:
+        console.log("othercases")
+    }
+}, 1000);
+
+
+window.addEventListener('load', (event) => {
+    
+    let heart = document.getElementById('heart');
+    console.log(document.getElementById('heart'));
+    
+    heart.style.color = "magenta";
+
+
+
+
+});
+
+
+
+
